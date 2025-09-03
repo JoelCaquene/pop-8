08b4fa005a2bee754cefcc3763b425de1cac70ae
JoelCaquene <joelcaquenejr@gmail.com>
2025-09-03 14:13:10 -0300

    Corrigindo nomes de módulos para pop-8

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('plataforma.urls')),  # Esta linha direciona a URL raiz para as URLs do seu app
+    path('', include('pop-8.urls')),  # Esta linha direciona a URL raiz para as URLs do seu app
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
